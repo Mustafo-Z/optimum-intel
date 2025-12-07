f742f0ae9250265b00c3469093d287c3e3b6ef26
Mustafo-Z <eparis@atlassian.com>
2025-12-08 00:14:06 +0400

    minor fix

--- tests/openvino/utils_tests.py
+++ tests/openvino/utils_tests.py
@@ -132,7 +132,7 @@ MODEL_NAMES = {
     "minicpmv": "optimum-intel-internal-testing/tiny-random-minicpmv-2_6",
     "minicpmo": os.getenv(
         "MINICPMO_MODEL_PATH",
-        "optimum-intel-internal-testing/tiny-random-MiniCPM-o-2_6-mini"
+        "optimum-intel-internal-testing/tiny-random-MiniCPM-o-2_6-6mb"
     ),    
     "mistral": "optimum-intel-internal-testing/tiny-random-mistral",
     "mistral-nemo": "optimum-intel-internal-testing/tiny-random-mistral-nemo",
